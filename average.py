963b9c71e60e0c01b0dd403dbd763a24ff7098de
jagatsai <jagatsai555@gmail.com>
2024-10-24 09:57:24 +0530

    Your descriptive commit message

---
+++ average.py
@@ -0,0 +1,10 @@
+a = int(input("enter number 1:"))
+b = int(input("enter number 2:"))
+c = int(input("enter number 3:"))
+print("number a is :",a)
+print("number b is :",b)
+print("number c is :",c)
+
+print("sum is:",a+b+c)
+
+print ("average is :",(a+b+c)/3)
